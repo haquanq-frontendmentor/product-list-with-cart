89386290e2d83dc19835f80e71b3dcdf8f6443e8
Ha Quanq <quangha0120010@gmail.com>
2025-07-29 01:48:44 +0700

    Add Vite plugin to copy static assets

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     root: "src/",
     build: { outDir: "../dist/", emptyOutDir: true },
     base: "/product-list-with-cart/",
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "assets/images",
+                    dest: "../dist/assets/",
+                },
+            ],
+        }),
+    ],
 });
