6bedbe3b17c7ce84c702b4a3546f161e70260d3d
Ha Quanq <quangha0120010@gmail.com>
2025-07-29 01:05:09 +0700

    Update Vite build public path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import { defineConfig } from "vite";
 export default defineConfig({
     root: "src/",
     build: { outDir: "../dist/", emptyOutDir: true },
-    base: "/product-list-with-card/",
+    base: "/product-list-with-cart/",
 });
