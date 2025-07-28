7df2b25d06fb3dbbebdc87f1ebee6394861f0792
Ha Quanq <quangha0120010@gmail.com>
2025-07-29 00:37:42 +0700

    Add modal for keyboard navigation guide

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,15 @@
 import "./features/cart";
 import "./features/product";
+import { createModal } from "./helpers/createModal";
+
+const guideModal = createModal({
+    wrapper: document.querySelector(".guide-modal") as HTMLDivElement,
+    container: document.querySelector(".guide-modal__container") as HTMLElement,
+});
+
+const guideModalDismissButton = document.querySelector(".guide-modal__dismiss-btn") as HTMLButtonElement;
+guideModalDismissButton.addEventListener("click", () => guideModal.close());
+
+setTimeout(() => {
+    guideModal.open();
+}, 700);
